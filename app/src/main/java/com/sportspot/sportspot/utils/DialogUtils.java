582beb17368f92802f57388831744e718c3785a2
csabarato <=>
2020-09-25 22:30:43 +0200

    Change Login and Reg form design to Android Material design.

--- app/src/main/java/com/sportspot/sportspot/utils/DialogUtils.java
+++ app/src/main/java/com/sportspot/sportspot/utils/DialogUtils.java
@@ -7,23 +7,21 @@ import android.view.ContextThemeWrapper;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.sportspot.sportspot.R;
 
 public class DialogUtils {
 
     public static AlertDialog buildAlertDialog(String title, String message, AppCompatActivity context) {
 
-        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(new ContextThemeWrapper(context, R.style.AlertDialogTheme));
-        alertBuilder.setTitle(title);
-        alertBuilder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-        alertBuilder.setMessage(message);
+        final MaterialAlertDialogBuilder matAlertDialogBuilder = new MaterialAlertDialogBuilder(context).setTitle(title).setMessage(message)
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
 
-        return alertBuilder.create();
+        return matAlertDialogBuilder.create();
     }
-
 }
